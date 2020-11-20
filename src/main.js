0f4b92c8f4ddd7ed8384b5895017ff0a4365dfa3
Lee Palacios <vitualizz@gmail.com>
2020-11-20 13:05:07 -0500

    add locale element ui

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css';
+import 'element-plus/lib/theme-chalk/index.css'
+import locale from 'element-plus/lib/locale/lang/es'
 
 const app = createApp(App)
 
 // Element
-app.use(ElementPlus)
+app.use(ElementPlus, { locale })
 
 // Directive
 app.directive('hidden', (el, binding) => {
