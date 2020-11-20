9c84a597a737a820011d77fe3110beb6c8fe25d2
Lee Palacios <vitualizz@gmail.com>
2020-11-20 12:55:24 -0500

    implement element

--- src/components/organisms/contact/contact.vue
+++ src/components/organisms/contact/contact.vue
@@ -6,6 +6,7 @@
       br
       span.f-bold DEPA!
   .o-contact__form
+    el-button gaaa
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Button, Input } from 'element-plus'
 
 const app = createApp(App)
+
+// Element
+const components = [Button, Input]
+components.forEach(component => app.use(component))
 
 // Directive
 app.directive('hidden', (el, binding) => {
