6d8aa6d0779dff2861ea6ba6d98c253b8621817d
Lee Palacios <vitualizz@gmail.com>
2020-11-22 19:30:48 -0500

    validations

--- src/components/organisms/contact/contact.vue
+++ src/components/organisms/contact/contact.vue
@@ -7,22 +7,41 @@
       span DEPA!
 
 
-  el-form(:modal='form').o-contact__form
-    el-form-item(label='Nombre')
+  el-form(
+    ref='form'
+    :model='form'
+    :rules='rules'
+  ).o-contact__form
+    el-form-item(
+      label='Nombre'
+      prop='name'
+    )
       el-input(v-model='form.name')
 
-    el-form-item(label='Celular')
-      el-input(v-model='form.phone')
+    el-form-item(
+      label='Celular'
+      prop='phone'
+    )
+      el-input(v-model.number='form.phone')
 
-    el-form-item(label='Email')
+    el-form-item(
+      label='Email'
+      prop='email'
+    )
       el-input(v-model='form.email')
 
-    el-form-item(label='DNI')
-      el-input(v-model='form.dni')
+    el-form-item(
+      label='DNI'
+      prop='document'
+    )
+      el-input(v-model.number='form.document')
 
-    el-form-item(label='Proyecto de Interés')
+    el-form-item(
+      label='Proyecto de Interés'
+      prop='project_related_id'
+    )
       el-select(
-        v-model='form.project_related_ids'
+        v-model='form.project_related_id'
         placeholder='Proyecto'
       )
         el-option(
@@ -31,7 +50,10 @@
           :value="`name${i}`"
         )
 
-    el-form-item(label='Comentario')
+    el-form-item(
+      label='Comentario'
+      prop='observation'
+    )
       el-input(
         type='textarea'
         v-model='form.observation'
@@ -39,7 +61,10 @@
       )
 
     el-form-item
-      el-button(type='primary') Enviar
+      el-button(
+        @click='onClickSubmit'
+        type='primary'
+      ) Enviar
 </template>
 
 <script>
@@ -51,10 +76,50 @@ export default {
         name: '',
         phone: '',
         email: '',
-        dni: '',
-        project_related_ids: [],
+        document: '',
+        project_related_id: null,
         observation: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: 'Ingresa tu nombre.', trigger: 'blur' }
+        ],
+        phone: [
+          { required: true, message: 'Ingresa tu número de celular.', trigger: 'blur' },
+          { type: 'number', message: 'Ingresa un número de celular válido', trigger: 'change' }
+        ],
+        email: [
+          { required: true, message: 'Ingresa tu email.', trigger: 'blur' },
+          { type: 'email', message: 'Ingresa un email válido', trigger: 'blur' }
+        ],
+        document: [
+          { required: true, message: 'Ingresa tu DNI.', trigger: 'blur' },
+          { type: 'number', message: 'Ingresa un número de documento válido', trigger: 'change' }
+        ],
+        project_related_id: [
+          { required: true, message: 'Selecciona un Proyecto de Interés.', trigger: 'change' }
+        ],
+        observation: [
+          { required: true, message: 'Ingresa un comentario.', trigger: 'blur' }
+        ],
       }
+    }
+  },
+  methods: {
+    onClickSubmit () {
+      window.asd = this.$refs['form']
+      this.$refs['form'].validate((valid) => {
+        console.log(valid)
+        if (valid) {
+          this.submitForm()
+        } else {
+          console.log('gaaa')
+          return false
+        }
+      })
+    },
+    submitForm () {
+      alert('Enviadooo')
     }
   }
 }
