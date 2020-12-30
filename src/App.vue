5082ef980db16dd07672a316f6defc22b109e436
Lee Palacios <vitualizz@gmail.com>
2020-12-30 10:50:03 -0500

    remove code unnecesary

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,6 @@ export default {
   name: 'App',
   components: {
     Home
-  },
-  mounted () {
-    this.axiosConfig()
-  },
-  methods: {
-    axiosConfig () {
-      const baseUrl = 'https://kuuost331l.execute-api.us-east-2.amazonaws.com/latest'
-      this.axios.defaults.baseURL = baseUrl
-    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ const app = createApp(App)
 // Axios
 const baseUrl = 'https://kuuost331l.execute-api.us-east-2.amazonaws.com/latest'
 axios.defaults.baseURL = baseUrl
-axios.defaults.headers.Authorization = '1b7e8fa834d376146f5024ab8767e1e54da6bdf01df76248d6bace11db5eb442'
 app.use(VueAxios, axios)
 
 // Element
